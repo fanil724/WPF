c35f6c51120889272bb2588c75be5d84c2c791d9
Фаниль <fanil724@gmail.com>
2023-08-30 21:47:41 +0300

    DataBinding done

---
+++ DataBinding/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Windows;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+namespace DataBinding
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+            txtInput.Focus();
+        }
+        private string boundtext;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string BoundText
+        {
+            get { return boundtext; }
+            set
+            {
+                boundtext = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void btnSet_Click(object sender, RoutedEventArgs e)
+        {
+            BoundText = "Set from code";
+        }
+    }
+}
